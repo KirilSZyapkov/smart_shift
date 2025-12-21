4120e4d0b344af04cb90a51c603bdd1c60fd9599
Kiril Zyapkov <appdeveloperjs47@gmail.com>
2025-12-21 12:28:18 +0200

    feat: setting up the backend server to run.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,29 @@
 import express from "express";
 import cors from "cors";
+// import {errorHandler} from "./middlewares/errorHandler";
+import {clerkMiddleware} from "@clerk/express";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+//todo: да активирам клърк app.use(clerkMiddleware());
+
+// app.use("/trpc",{
+//   router: appRouter,
+//
+// });
+
+app.get("/favicon.ico", (req, res)=> res.status(204).end());
+app.get("/",(req, res)=>{
+  res.send("Welcome, server is running!");
+});
+
+// app.use(errorHandler);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, ()=>{
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
