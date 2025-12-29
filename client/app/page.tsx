40462675e98230a4f49d96757fe4d635b46dbbae
Kiril Zyapkov <appdeveloperjs47@gmail.com>
2025-12-29 13:34:44 +0200

    feat: style the login form.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,60 +1,13 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import {
-  Field,
-  FieldDescription,
-  FieldGroup,
-  FieldLabel,
-  FieldSeparator,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+import { GalleryVerticalEnd } from "lucide-react"
 
-export default function LoginFormPage() {
+import { LoginForm } from "@/components/login-form"
+
+export default function LoginPage() {
   return (
-    <div className={cn("flex flex-col gap-6 container m-auto max-w-2xl", )} >
-      <Card>
-        <CardHeader className="text-center">
-          <CardTitle className="text-xl">Welcome back</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <FieldGroup>
-              <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
-                Login with your work account
-              </FieldSeparator>
-              <Field>
-                <FieldLabel htmlFor="email">Email</FieldLabel>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </Field>
-              <Field>
-                <div className="flex items-center">
-                  <FieldLabel htmlFor="password">Password</FieldLabel>
-                </div>
-                <Input id="password" type="password" required />
-              </Field>
-              <Field>
-                <Button type="submit">Login</Button>
-              </Field>
-            </FieldGroup>
-          </form>
-        </CardContent>
-      </Card>
-      <FieldDescription className="px-6 text-center">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
-      </FieldDescription>
+    <div className="bg-muted flex min-h-svh  items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <LoginForm />
+      </div>
     </div>
   )
 }
